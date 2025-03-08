e05c245c636c5f493572e63bbc1f84b81ca141da
Vishnu1604-github <youremail@example.com>
2025-03-07 19:53:40 -0500

    intiial commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/vishnumandula/Downloads/My-portfolio/venv"
+set -gx VIRTUAL_ENV "/Users/vishnumandula/Downloads/My-portfolio-website/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
